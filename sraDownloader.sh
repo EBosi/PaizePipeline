298a70d1c03bb99e56e03a4bfa3920137e52fb71
EBosi <bosiemanuele@gmail.com>
2016-11-04 13:57:05 +0100

    supported sra-toolbox for downloading reads

---
+++ sraDownloader.sh
@@ -0,0 +1,21 @@
+usage="bash sraDownloader.sh ACCESSION OUTDIR"
+
+# standard input
+accession=$1
+outdir=$2
+
+# change accordingly to your system (require aspera connect to be installed)
+asperaExec=$HOME/.aspera/connect/bin/ascp
+asperaKey=$HOME/.aspera/connect/etc/asperaweb_id_dsa.openssh
+sraOutDir=$HOME/ncbi/public/sra
+
+# download .sra file for $accession
+clPrefetch="prefetch -t ascp -a \"$asperaExec|$asperaKey\" $accession"
+echo "prefetch cl used: $clPrefetch"
+eval $clPrefetch
+sraOutFile=$sraOutDir/$accession.sra
+
+# extract splitted .fastq reads from .sra
+if [[ -e $outdir ]]; then rm -rf $outdir; fi
+fastq-dump $sraOutFile --split-files -O $outdir --gzip
+rm $sraOutFile
